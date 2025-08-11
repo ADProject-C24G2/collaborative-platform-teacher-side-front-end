c9dae698ac7b1d291d8e40cf679885f5f70fcca0
Jason Wong <86539564+Aintor@users.noreply.github.com>
2025-08-11 22:47:12 +0800

    update vercel.json

--- teacher-side-front-end/src/app.tsx
+++ teacher-side-front-end/src/app.tsx
@@ -181,13 +181,3 @@ export const layout: RunTimeLayoutConfig = ({
     ...initialState?.settings,
   };
 };
-
-/**
- * @name request 配置，可以配置错误处理
- * 它基于 axios 和 ahooks 的 useRequest 提供了一套统一的网络请求和错误处理方案。
- * @doc https://umijs.org/docs/max/request#配置
- */
-// export const request: RequestConfig = {
-//   baseURL: isDev ? "" : "https://proapi.azurewebsites.net",
-//   ...errorConfig,
-// };
